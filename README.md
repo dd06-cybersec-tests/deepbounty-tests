d11b0faa94c191f57f71190defecec5f24e4b3ce
dd060606 <dd06.developpement@gmail.com>
2026-01-12 16:41:15 +0100

    setup test files

---
+++ README.md
@@ -0,0 +1,4 @@
+## Legacy Documentation
+
+For more details, visit our old documentation site:
+[https://www.example.com/v1/docs/broken-link](https://www.example.com/v1/docs/broken-link)
---
+++ app.py
@@ -0,0 +1,8 @@
+def main():
+    LINK = "https://github.com/dev-ops-ninja-2023/myrepo" # Vulnerable example
+    print("EEEEEEEEEEEE: ${https://github.com/dev-ops-ninja-2025}")
+    s3 = "https://deprovisioned-marketing-assets.s3.amazonaws.com/logo.png"
+    gcs = "https://storage.googleapis.com/abandoned-internal-backups/config.json"
+
+
+main()
