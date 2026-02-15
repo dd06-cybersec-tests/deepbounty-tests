c3b33a6cb624f5da61312f8bc5b4b4bc822c5cd3
dd060606 <dd06.developpement@gmail.com>
2026-02-15 11:11:34 +0100

    Add curl command to fetch Ruby CI logs in test script

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 curl https://github.com/dev-ninja-189/myrepo
 
 wget https://raw.githubusercontent.com/dd06-cybsersec-test/deepbounty-tests/refs/heads/main/Dockerfile.dev
+
+curl http://rubyci.s3.amazonaws.com/rubyci-logs/2024-06-01/1.log
